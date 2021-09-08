24ab31874911580a9a82e4e5d6321e5329a4b006
Ryan Bollettino <ryan.bollettino@gmail.com>
2021-09-08 00:57:35 -0500

    initial migration

---
+++ db/schema.rb
@@ -0,0 +1,39 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2021_09_08_000318) do
+
+  create_table "characters", force: :cascade do |t|
+    t.string "name"
+    t.datetime "debut"
+    t.text "review"
+    t.string "image"
+    t.string "script_one"
+    t.string "script_two"
+    t.string "script_three"
+    t.integer "likes"
+    t.integer "member_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "members", force: :cascade do |t|
+    t.string "first"
+    t.string "last"
+    t.string "joined"
+    t.string "left"
+    t.string "image"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+end
