6206a2384d778b99c785c1c9c0b8070d9592492e
Ryan Bollettino <ryan.bollettino@gmail.com>
2021-09-07 21:12:34 -0500

    t.integer :member_id instead of references

--- db/migrate/20210908000318_create_characters.rb
+++ db/migrate/20210908000318_create_characters.rb
@@ -6,7 +6,7 @@ class CreateCharacters < ActiveRecord::Migration[6.1]
       t.text :review
       t.string :image
       t.integer :likes
-      t.string :member
+      t.integer :member_id
 
       t.timestamps
     end
