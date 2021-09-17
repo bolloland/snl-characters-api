9199e969b6d7f9512075d0d802e747db91f4a25b
Ryan Bollettino <ryan.bollettino@gmail.com>
2021-09-17 10:14:13 -0500

    conditional rendering Member.all & @member

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -5,12 +5,13 @@ class MembersController < ApplicationController
   def index
     members = Member.all
 
-    render json: members
+    render json: members, except: [:created_at, :updated_at], include: [characters: {except: [:created_at, :updated_at]}]
+
   end
 
   # GET /members/1
   def show
-    render json: member
+    render json: @member, except: [:created_at, :updated_at], include: [characters: {except: [:created_at, :updated_at]}]
   end
 
   # POST /members
@@ -26,23 +27,23 @@ class MembersController < ApplicationController
 
   # PATCH/PUT /members/1
   def update
-    if member.update(member_params)
-      render json: member
+    if @member.update(member_params)
+      render json: @member
     else
-      render json: member.errors, status: :unprocessable_entity
+      render json: @member.errors, status: :unprocessable_entity
     end
   end
 
   # DELETE /members/1
   def destroy
-    member.destroy
+    @member.destroy
     render json: {message: "You've deleted successfully!"}
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_member
-      member = Member.find(params[:id])
+      @member = Member.find(params[:id])
     end
 
     # Only allow a list of trusted parameters through.
